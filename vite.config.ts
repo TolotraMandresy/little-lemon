d452198b8f01b4e5602eac2f944893c0b9efb189
Tolotra Mandresy <tolotramandresy39@gmail.com>
2025-01-30 15:37:33 +0300

    added scss

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
+import sass from 'sass'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +9,11 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        implementation: sass,
+      },
+    },
+  },
 })
