001d2fc9bf6365bae981f4e8db7873016f5db3c3
Tolotra Mandresy <tolotramandresy39@gmail.com>
2025-02-17 17:43:20 +0300

    added svg mask

--- src/layout/Authentification/Authentification.tsx
+++ src/layout/Authentification/Authentification.tsx
@@ -6,13 +6,13 @@ export default function Authentification({ children }: { children: React.ReactNo
     return (
         <div className="h-screen flex flex-col items-center gap-4">
             <div className="flex illustr-container w-full h-[30vh] max-h-[270px] relative">
-                <img src="../../../public/img/illustration.jpg" className="flex h-full w-full rounded-b-[50%] -z-10" alt="Little Lemon Illustration" />
+                <img src="../../../public/img/illustration.jpg" className="flex h-full w-full -z-10" alt="Little Lemon Illustration" />
                 <div id="top-logo" className="rounded-md px-4 py-2 backdrop-opacity-60 backdrop-blur-xs absolute top-[50%] left-[50%] -translate-[50%]">
                     <img src="../../../public/icons_assets/Logo.svg" alt="logo" />
                 </div>
             </div>
 
-            <div className='h-full w-full max-w-[350px] justify-between px-4'>
+            <div className='h-full w-full max-w-[380px] justify-between px-4'>
                 {children}
             </div>
         </div>
