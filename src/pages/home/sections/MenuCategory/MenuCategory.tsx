c2d4d4c2e19acf4f20c047b797597d7909cd3433
Tolotra Mandresy <tolotramandresy39@gmail.com>
2025-02-21 15:01:57 +0300

    fix menucategory

--- src/pages/home/sections/MenuCategory/MenuCategory.tsx
+++ src/pages/home/sections/MenuCategory/MenuCategory.tsx
@@ -6,10 +6,10 @@ export default function MenuCategory({className} : {className?: string}) {
         { selectedCateg, addCategory, removeCategory } = useCategContext() as ICategoryContext        
 
     return (
-        <div className={`flex flex-col gap-2 w-full overflow-auto ${className}`}>
-            <p className="font-bold">Order for delivery!</p>
+        <div className={`flex flex-col gap-2 w-full ${className}`}>
+            <p className="font-extrabold text-2xl">Order for delivery!</p>
 
-            <div className="flex flex-row gap-2">
+            <div className="flex flex-row gap-2 w-full overflow-auto">
                 {categList.map(element => (
                     <Category label={element} key={element} addCategory={addCategory} removeCategory={removeCategory} isChecked={selectedCateg.has(element)}/>
                 ))}
