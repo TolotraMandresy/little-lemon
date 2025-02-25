3fa026077ddc256aa0a67a58e52f07d7f980f749
Tolotra Mandresy <tolotramandresy39@gmail.com>
2025-02-25 18:53:30 +0300

    fix gap-y in main

--- src/pages/home/sections/MenuCategory/MenuCategory.tsx
+++ src/pages/home/sections/MenuCategory/MenuCategory.tsx
@@ -6,7 +6,7 @@ export default function MenuCategory({className} : {className?: string}) {
         { selectedCateg, addCategory, removeCategory } = useCategContext() as ICategoryContext        
 
     return (
-        <div className={`flex flex-col gap-2 w-full ${className}`}>
+        <div className={`flex flex-col gap-4 w-full ${className}`}>
             <p className="font-extrabold text-2xl">Order for delivery!</p>
 
             <div className="flex flex-row gap-2 w-full overflow-auto">
