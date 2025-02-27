f1bb0c405ed31861a471cf1574fbe399ecc24230
Tolotra Mandresy <tolotramandresy39@gmail.com>
2025-02-27 11:38:23 +0300

    banner responsive

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -13,10 +13,10 @@ function Content() {
 
     return (
         <WithHeader className="max-w-[900px] xl:max-w-[1200px] px-2">
-            <div className="flex flex-row w-full gap-4 justify-between px-4 py-8 relative">
+            <div className="flex flex-row w-full gap-4 justify-around px-4 py-8 relative md:px-8 md:py-16">
                 <img id="bgimg" src="/img/illustr.webp" className="rounded-md absolute -z-0 top-0 left-0 w-screen h-full" alt="illustration" />
 
-                <div className="flex flex-col gap-4 w-[60%] max-w-[250px] z-0" id="banner">
+                <div className="flex flex-col lg:justify-center gap-4 w-[60%] max-w-[250px] lg:max-w-[400px] z-0" id="banner">
                     <div className="flex flex-col">
                         <h1 className="text-c-yellow text-7xl whitespace-nowrap">Little Lemon</h1>
                         <h2 className="text-5xl text-c-font-secondary">Chicago</h2>
@@ -28,8 +28,8 @@ function Content() {
 
                     <PrimaryButton onClick={openSheet} label="Reserve a table" className="mt-12" style={{ 'maxWidth': 'max-content' }} />
                 </div>
-                <div className="flex items-center w-[40%] max-w-[180px] translate-y-3">
-                    <img className="w-full h-max aspect-square rounded-sm" src="/img/restauranfood_20.webp" alt="foods" />
+                <div className="flex items-center w-[40%] max-w-[180px] lg:max-w-[unset] lg:max-h-[300px] translate-y-3 ">
+                    <img className="ml-auto w-full h-max lg:h-full lg:w-auto aspect-square rounded-sm" src="/img/restauranfood_20.webp" alt="foods" />
                 </div>
                 <BottomSheet />
             </div>
